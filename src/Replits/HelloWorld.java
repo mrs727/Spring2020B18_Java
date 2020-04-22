4a95d7396f793f6da6b29126f27d1e2e833f27ca
Madina Shakirova <mrs727@humboldt.edu>
2020-04-21 19:37:18 -0700

    second commit

---
+++ src/Replits/HelloWorld.java
@@ -0,0 +1,11 @@
+package Replits;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World");
+
+        System.out.println("Shakirova, Madina");
+
+
+    }
+}
